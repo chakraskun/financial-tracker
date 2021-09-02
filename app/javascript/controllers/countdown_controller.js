ea5c29c648e24bc7a2519941247135eafa55f994
Chakras Andika <chakras_campur@gmail.com>
2021-09-02 18:27:41 +0700

    add countdown

---
+++ app/javascript/controllers/countdown_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  connect(){
+    this.timer = document.querySelector('.js-countdown')
+  }
+
+  initialize(){
+    var timer = document.querySelector('.js-countdown')
+    var countDownDate = new Date("Dec 11, 2021 10:00:00").getTime();
+    var x = setInterval(function() {
+
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      timer.innerHTML = days + " Days, " + hours + " Hours, " + minutes + " Minutes ";
+        
+      if (distance < 0) {
+        clearInterval(x);
+        timer.innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  }
+}

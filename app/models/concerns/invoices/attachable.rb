38f2a9eb3d98fa4caaa8265330efd1ee4ca0dc1b
Chakras <chakras.campur@gmail.com>
2022-02-13 17:31:26 +0700

    add error heroku

--- app/models/concerns/invoices/attachable.rb
+++ app/models/concerns/invoices/attachable.rb
@@ -2,8 +2,7 @@ module Invoices
   module Attachable extend ActiveSupport::Concern
   
     def attachment
-      return @attachment if @attachment.present?
-      @attachment = InvoiceAttachment
+      InvoiceAttachment
         .find_by(id: self.invoice_attachment_id)
         .proof_of_transaction
     end

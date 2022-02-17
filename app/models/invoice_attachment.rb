a6cc7939a5d82ef27a7401ea80330dd583e879e5
Chakras <chakras.campur@gmail.com>
2022-02-17 13:19:39 +0700

    add service s3 and add validation

--- app/models/invoice_attachment.rb
+++ app/models/invoice_attachment.rb
@@ -1,3 +1,12 @@
 class InvoiceAttachment < ApplicationRecord
-  has_one_attached :proof_of_transaction
+  has_one_attached :proof_of_transaction, service: :s3
+
+  validate :acceptable_image
+
+  def acceptable_image
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(proof_of_transaction.content_type)
+      errors.add(:proof_of_transaction, "must be a JPEG or PNG")
+    end
+  end
 end
